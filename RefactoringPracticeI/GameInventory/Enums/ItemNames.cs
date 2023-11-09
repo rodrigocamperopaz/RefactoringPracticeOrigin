a23ff05707705a5978f5fb988a12c5b3ee7c49e9
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-11-09 12:13:03 -0400

    Enum for item names added

---
+++ RefactoringPracticeI/GameInventory/Enums/ItemNames.cs
@@ -0,0 +1,10 @@
+namespace RefactoringExerciseI.Enums;
+public static class ItemNames
+{
+    public const string AgedBrie = "Aged Brie";
+    public const string BackstagePasses = "Backstage passes to a Pokemon Gym concert";
+    public const string SulfurasHand = "Sulfuras, Hand of Ragnaros";
+    public const string ElixirMongoose = "Elixir of the Mongoose";
+    public const string ConjuredManaCake = "Conjured Mana Cake";
+    public const string Plus5DxtVest = "+5 Dexterity Vest";
+}
