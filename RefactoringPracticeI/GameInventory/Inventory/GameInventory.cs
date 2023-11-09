dd2b033f4de53a1974e63041cf655e4651ff5457
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-11-09 18:41:27 -0400

    Game Inventory now uses an Item update chain to update its items

--- RefactoringPracticeI/GameInventory/Inventory/GameInventory.cs
+++ RefactoringPracticeI/GameInventory/Inventory/GameInventory.cs
@@ -1,14 +1,16 @@
-﻿using RefactoringExerciseI.Enums;
+﻿using RefactoringExerciseI.Inventory.Handlers;
 
 namespace RefactoringExerciseI.Inventory
 {
     public class GameInventory
     {
         private readonly IList<Item> _items;
+        private readonly ItemUpdateChain _updateChain;
 
         public GameInventory(IList<Item> Items)
         {
             _items = Items;
+            _updateChain = new ItemUpdateChain();
         }
 
         public void UpdateQuality()
@@ -16,57 +18,7 @@ namespace RefactoringExerciseI.Inventory
             for (int index = 0; index < _items.Count; index++)
             {
                 Item currentItem = _items[index];
-                if (currentItem.Name != ItemNames.AgedBrie && currentItem.Name != ItemNames.BackstagePasses)
-                {
-                    if (currentItem.Quality > 0 && currentItem.Name != ItemNames.SulfurasHand)
-                    {
-                        currentItem.Quality--;
-                    }
-                }
-                else if (currentItem.Quality < 50)
-                {
-                    currentItem.Quality++;
-
-                    if (currentItem.Name == ItemNames.BackstagePasses)
-                    {
-                        if (currentItem.SellIn < 11 && currentItem.Quality < 50)
-                        {
-                            currentItem.Quality++;
-                        }
-
-                        if (currentItem.SellIn < 6 && currentItem.Quality < 50)
-                        {
-                            currentItem.Quality++;
-                        }
-                    }
-                }
-
-                if (currentItem.Name != ItemNames.SulfurasHand)
-                {
-                    currentItem.SellIn--;
-                }
-
-                if (currentItem.SellIn < 0)
-                {
-                    if (currentItem.Name != ItemNames.AgedBrie)
-                    {
-                        if (currentItem.Name != ItemNames.BackstagePasses)
-                        {
-                            if (currentItem.Quality > 0 && currentItem.Name != ItemNames.SulfurasHand)
-                            {
-                                currentItem.Quality--;
-                            }
-                        }
-                        else
-                        {
-                            currentItem.Quality = 0;
-                        }
-                    }
-                    else if (currentItem.Quality < 50)
-                    {
-                        currentItem.Quality++;
-                    }
-                }
+                _updateChain.UpdateItem(currentItem);
             }
         }
     }
