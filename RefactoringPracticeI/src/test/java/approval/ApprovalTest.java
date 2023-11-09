09c75078ef4436ed023411040cb43466e8bf2464
JoseLozadaAD <jose.lozada@training.assuresoft.com>
2023-11-09 10:16:22 -0400

    Fixed ApprovalTest

--- RefactoringPracticeI/src/test/java/approval/ApprovalTest.java
+++ RefactoringPracticeI/src/test/java/approval/ApprovalTest.java
@@ -1,12 +1,13 @@
 package approval;
 
 import org.assuresoft.Program;
+import org.assuresoft.utils.TestConstants;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ApprovalTest {
   @Test
@@ -18,7 +19,9 @@ public class ApprovalTest {
     Program.main(new String[0]);
 
     String output = fakeOutput.toString();
+    // Use to convert LF instead to CRLF
+    output = output.replace("\r\n", "\n");
 
-    assertNotNull(output);
+    assertEquals(TestConstants.EXPECTED_PROGRAM_OUTPUT, output);
   }
 }
