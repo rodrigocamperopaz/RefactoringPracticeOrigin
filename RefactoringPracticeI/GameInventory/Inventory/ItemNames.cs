4f140884b94b6abc37fa2438ec0dbacd02643c0e
JoDemVel <josuedemetriovelizescobar@gmail.com>
2023-11-09 18:46:37 -0400

    Add method factory pattern to create handler and constants in ItemNmes

---
+++ RefactoringPracticeI/GameInventory/Inventory/ItemNames.cs
@@ -0,0 +1,17 @@
+﻿namespace RefactoringExerciseI.Inventory
+{
+    public static class ItemNames
+    {
+        public const string DexterityVest = "+5 Dexterity Vest";
+        public const string AgedBrie = "Aged Brie";
+        public const string ElixirOfTheMongoose = "Elixir of the Mongoose";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string BackstagePasses = "Backstage passes to a Pokemon Gym concert";
+        public const string ConjuredManaCake = "Conjured Mana Cake";
+        public static readonly string[] Standars = {
+            DexterityVest,
+            ElixirOfTheMongoose,
+            ConjuredManaCake
+        };
+    }
+}
